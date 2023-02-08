8b780291ff5e4a7593fef5e8146c0bda87c2a9c2
joliv3r <jacobbruun@hotmail.no>
2023-02-08 17:51:53 +0100

    started A1 in optics

---
+++ optikk/assignment1/assignment1.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def refraction(n_i, n_t, theta):
+    temp = n_i/n_t * np.sin(theta)
+    for i, j in enumerate(temp):
+        if j > 1:
+            temp[i] = 1
+    return np.arcsin(temp)
+
+
+def reflection(n_i, theta_i, n_t, theta_t, pol):
+    if pol == 'p':
+        return ((n_t * np.cos(theta_i) - n_i * np.cos(theta_t)) / (n_i * np.cos(theta_t) + n_t * np.cos(theta_i)))**2
+    if pol == 's':
+        return ((n_i * np.cos(theta_i) - n_t * np.cos(theta_t)) / (n_i * np.cos(theta_i) + n_t * np.cos(theta_t)))**2
+    return 0
+
+
+n_w, n_a = 1.334, 1
+theta = np.linspace(0, np.pi/2, 100, endpoint=False)
+
+plt.plot(theta, reflection(n_a, theta, n_w, refraction(n_a, n_w, theta), 's'), 'b', label='s-polarized from air to water')
+plt.plot(theta, reflection(n_a, theta, n_w, refraction(n_a, n_w, theta), 'p'), 'r', label='p-polarized from air to water')
+plt.plot(theta, reflection(n_w, theta, n_a, refraction(n_w, n_a, theta), 's'), 'y', label='s-polarized from water to air')
+plt.plot(theta, reflection(n_w, theta, n_a, refraction(n_w, n_a, theta), 'p'), 'k', label='p-polarized from water to air')
+
+plt.plot(theta, 1-reflection(n_a, theta, n_w, refraction(n_a, n_w, theta), 's'), 'b--')
+plt.plot(theta, 1-reflection(n_a, theta, n_w, refraction(n_a, n_w, theta), 'p'), 'r--')
+plt.plot(theta, 1-reflection(n_w, theta, n_a, refraction(n_w, n_a, theta), 's'), 'y--')
+plt.plot(theta, 1-reflection(n_w, theta, n_a, refraction(n_w, n_a, theta), 'p'), 'k--')
+
+plt.legend()
+plt.show()
